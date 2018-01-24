f439bd17c6b45c49bc9033aa3d6489a07d67a606
Iron Riders <software@ironriders.org>
2018-01-23 16:42:22 -0800

    drive train class added

---
+++ src/frc/team4180/DriveTrain.java
@@ -0,0 +1,45 @@
+package frc.team4180;
+
+//package org.usfirst.frc.team4180.robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DriveTrain {
+    private VictorSP leftVictor, rightVictor;
+    private boolean backwards = false;
+    private DoubleSolenoid gearShifting;
+    private boolean state = false;
+
+    public DriveTrain(int leftPort, int rightPort, int gearShiftPort1, int gearShiftPort2) {
+        leftVictor = new VictorSP(leftPort);
+        rightVictor = new VictorSP(rightPort);
+        gearShifting = new DoubleSolenoid(gearShiftPort1, gearShiftPort2);
+    }
+
+    public void updateSpeed(/*LambdaJoystick.ThrottlePosition throttlePosition*/) {
+        double left =0;// -throttlePosition.y - throttlePosition.x;
+        double right = 0;//throttlePosition.y - throttlePosition.x;
+        leftVictor.set(backwards ? left : right);
+        rightVictor.set(backwards ? right : left);
+    }
+
+    public void toggleGearShifting() {
+        setGear(!state);
+    }
+
+    public void setGear(boolean b) {
+        state = b;
+        SmartDashboard.putString("DB/String 8", state ? "Fast" : "Slow");
+        gearShifting.set(state ? DoubleSolenoid.Value.kReverse : DoubleSolenoid.Value.kForward);
+    }
+
+    public void toggleBackwards() {
+        backwards = !backwards;
+    }
+
+    public void setBackwards(boolean backwards){
+        this.backwards = backwards;
+    }
+}
