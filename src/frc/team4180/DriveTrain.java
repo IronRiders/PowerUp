5676d356604cd1e18febba2b0c2444268a0a354b
Ryan Hoffman <ryanfhoffman@gmail.com>
2018-01-25 16:02:54 -0800

    changed victorsp to speedcontroller

--- src/frc/team4180/DriveTrain.java
+++ src/frc/team4180/DriveTrain.java
@@ -3,24 +3,25 @@ package frc.team4180;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class DriveTrain {
-    private VictorSP leftVictor, rightVictor;
+    private SpeedController leftMotor, rightMotor;
     private boolean backwards = false;
     private DoubleSolenoid gearShifting;
     private boolean state = false;
 
     public DriveTrain(int leftPort, int rightPort, int gearShiftPort1, int gearShiftPort2) {
-        leftVictor = new VictorSP(leftPort);
-        rightVictor = new VictorSP(rightPort);
+        leftMotor = new VictorSP(leftPort);
+        rightMotor = new VictorSP(rightPort);
         gearShifting = new DoubleSolenoid(gearShiftPort1, gearShiftPort2);
     }
 
     public void updateSpeed(LambdaJoystick.ThrottlePosition throttlePosition) {
         double left = -throttlePosition.y - throttlePosition.x;
         double right = throttlePosition.y - throttlePosition.x;
-        leftVictor.set(backwards ? left : right);
-        rightVictor.set(backwards ? right : left);
+        leftMotor.set(backwards ? left : right);
+        rightMotor.set(backwards ? right : left);
     }
 
     public void toggleGearShifting() {
