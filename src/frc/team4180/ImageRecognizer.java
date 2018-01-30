6495e8ff8077f780bebf00a3a7c7e293b7f8df3f
corinneherzog <corinneh2001@gmail.com>
2018-01-29 20:05:49 -0800

    Started image recognizer class -Corinne

---
+++ src/frc/team4180/ImageRecognizer.java
@@ -0,0 +1,18 @@
+package frc.team4180;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+import static edu.wpi.first.wpilibj.networktables.NetworkTable.getTable;
+
+public class ImageRecognizer {
+    NetworkTable networkTable;
+
+
+    public ImageRecognizer(){
+        networkTable = NetworkTableInstance.getDefault().getTable("GRIP/myContoursReport");
+
+    }
+
+
+}
