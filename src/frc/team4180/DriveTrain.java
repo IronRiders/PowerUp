9d714350c4864bebf29ae31189295750565f0e2a
corinneherzog <corinneh2001@gmail.com>
2018-02-05 20:23:59 -0800

    Added position tracking code + added encoder code

--- src/frc/team4180/DriveTrain.java
+++ src/frc/team4180/DriveTrain.java
@@ -1,14 +1,22 @@
 package frc.team4180;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.VictorSP;
 
 public class DriveTrain {
     private SpeedController leftMotor, rightMotor;
+    private Encoder encoder;
+    final int wheelDiameter = 8;
+    final int pulsesPerRevolution = 360;
+
 
     public DriveTrain(int leftPort, int rightPort) {
-       // leftMotor = new VictorSP(leftPort);
-        //rightMotor = new VictorSP(rightPort);
+        leftMotor = new VictorSP(leftPort);
+        rightMotor = new VictorSP(rightPort);
+        encoder = new Encoder(1,0);
+        //encoder.reset();
+        encoder.setDistancePerPulse(Math.PI * wheelDiameter);
     }
 
     public void updateSpeed(LambdaJoystick.ThrottlePosition throttlePosition) {
@@ -16,5 +24,14 @@ public class DriveTrain {
         double right = throttlePosition.y - throttlePosition.x;
         leftMotor.set(right);
         rightMotor.set(left);
+    }
+
+    public double getDistance(){
+        int encoderTest = encoder.get();
+        System.out.print(encoderTest);
+        // 8 inches
+        double distance = encoder.getDistance();
+        //System.out.println(distance/pulsesPerRevolution);
+        return distance;
     }
 }
--- src/frc/team4180/Robot.java
+++ src/frc/team4180/Robot.java
@@ -1,22 +1,35 @@
 package frc.team4180;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import static frc.team4180.AutonomousMode.*;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.io.IOException;
+
 public class Robot extends IterativeRobot 
 {
-    private final DriveTrain driveTrain = new DriveTrain(-1, -1);
-    private final PositioningSystem positioningSystem = new PositioningSystem();
-
+    private final int LEFT_DRIVING = 0; //Placeholder
+    private final int RIGHT_DRIVING = 1; //Placeholder
+    private DriveTrain driveTrain;
+    private PositioningSystem positioningSystem;
     @Override
-    public void robotInit() 
+    public void robotInit()
     {
-        AutonomousMode.initializeNetworkTables(DO_NOTHING);
+
+       driveTrain = new DriveTrain(LEFT_DRIVING, RIGHT_DRIVING);
+       positioningSystem = new PositioningSystem();
+
+        CameraServer.getInstance().startAutomaticCapture();
     }
 
     @Override
     public void autonomousInit() 
     {
+        AutonomousMode.initializeNetworkTables(DO_NOTHING);
     }
 
     @Override
@@ -31,19 +44,32 @@ public class Robot extends IterativeRobot
     }
 
     @Override
-    public void teleopPeriodic() 
-    {
+    public void teleopPeriodic() {
+
+        // test for networktable
+//        double x = 0.33;
+//        while (x<50) {
+//            Timer.delay(1);
+//            x += 1;
+//            try {
+//                SmartDashboard.putNumber("/SmartDashboard/X", x);
+//
+//            } catch (NullPointerException e) {
+//                System.out.print(e);
+//            }
+//        }
+
+
+
+        positioningSystem.increment();
     }
 
     @Override
-    public void testPeriodic() 
-    {
-        positioningSystem.riemann();
-        positioningSystem.logPosition();
+    public void testPeriodic() {
+        driveTrain.getDistance();
     }
 
     @Override
-    public void testInit()
-    {
-    }
+    public void testInit() {}
+
 }
