7b896be7def9330d460852bdf54a09cec7a4dc64
Iron Riders <software@ironriders.org>
2018-02-06 16:26:34 -0800

    basic outline

---
+++ src/frc/team4180/UpperPiston.java
@@ -0,0 +1,17 @@
+package frc.team4180;
+
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class UpperPiston {
+    Solenoid CubePusher;
+
+    public UpperPiston(int port){
+        CubePusher = new Solenoid(port);
+    }
+    public extend() {
+
+    }
+    public reset() {
+
+    }
+}
