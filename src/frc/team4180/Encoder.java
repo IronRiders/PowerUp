363eb81fd921ca5d9aad6e95bba98151c052bff6
Iron Riders Student <software@ironriders.org>
2018-02-15 17:09:34 -0800

    simple plans for what the encoder can do

---
+++ src/frc/team4180/Encoder.java
@@ -0,0 +1,14 @@
+package frc.team4180;
+
+public class Encoder {
+    /**
+     * plans:
+     *
+     * values that can be called form Encoder:
+     * int count: number of rotations since last direction change
+     * double distance: the count times the scale factor
+     * double rate: the speed of the robot in M/S
+     * boolean stopped: does the speed equal 0
+     * boolean direction: forward of backward
+     */
+}
