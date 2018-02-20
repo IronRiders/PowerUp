166e4eb8bb134f26438056546b856b2169ae408d
Alex MacLean <alex.maclean2000@gmail.com>
2018-02-20 11:31:02 -0800

    Made it work

--- src/frc/team4180/Ports.java
+++ src/frc/team4180/Ports.java
@@ -1,10 +1,14 @@
 package frc.team4180;
 
 public class Ports {
-    // None of these have been tested
-    public static final int LEFT_DRIVING = 0;
-    public static final int RIGHT_DRIVING = 1;
-    public static final int LEFT_FLY_WHEEL = 2;
-    public static final int RIGHT_FLY_WHEEL = 3;
-    public static final int PISTON = 0;
+    public static final int LEFT_DRIVING_1 = 9;
+    public static final int LEFT_DRIVING_2 = 8;
+    public static final int RIGHT_DRIVING_1 = 7;
+    public static final int RIGHT_DRIVING_2 = 6;
+
+    public static final int LEFT_FLY_WHEEL = 5;
+    public static final int RIGHT_FLY_WHEEL = 4;
+
+    public static final int SOLENIOD_2 = 0;
+    public static final int SOLENIOD_1 = 1;
 }
